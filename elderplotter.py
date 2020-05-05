585e7493defb46ad0e85157ac7bdc4911f68f428
liaolimin_lovely@163.com <liaolimin_lovely@163.com>
2020-05-05 21:52:36 +0800

    增加数据图表

--- elderplotter.py
+++ elderplotter.py
@@ -4,7 +4,6 @@ import numpy as np
 
 from pyecharts import options as opts
 from pyecharts.charts import Pie, Bar, Page
-from pyecharts.charts import Bar
 from Csv import CSVPlotter
 
 my_c = CSVPlotter('older.csv')
@@ -17,14 +16,30 @@ ff = my_c.get2dcolumn(index)
 c = (
     Pie()
     .add("", [list(z) for z in zip(cc['住房情况'].keys(), cc['住房情况'].values())])
-    .set_global_opts(title_opts=opts.TitleOpts(title="Pie-住房情况"))
+    .set_global_opts(title_opts=opts.TitleOpts(title="Pie-住房情况"),
+                     legend_opts=opts.LegendOpts(type_="scroll",pos_right="20%",orient="horizontal"))
     .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
-    .render("pie_region.html")
+    #.render("pie_region.html")
+)
+
+c2= (
+    Pie()
+    .add("", [list(z) for z in zip(cc['性别'].keys(), cc['性别'].values())])
+    .set_global_opts(title_opts=opts.TitleOpts(title="Pie-性别比例"))
+    .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    #.render("pie_region.html")
 )
 
 bar = Bar()
 bar.add_xaxis(sorted(ff[0]))
 for i in range(len(ff[4])):
     bar.add_yaxis(ff[4][i], ff[1][i].tolist())
-bar.set_global_opts(title_opts=opts.TitleOpts(title="自理情况和居住状况"))
-bar.render()
+bar.set_global_opts(title_opts=opts.TitleOpts(title="自理情况和居住状况"),
+                    legend_opts=opts.LegendOpts(type_="scroll",pos_left="80%",orient="vertical"))
+#bar.render()
+
+page = Page()
+page.add(
+    bar, c,c2
+)
+page.render("page_simple_layout.html")
